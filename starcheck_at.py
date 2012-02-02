e2c34f049a1fcd13027dcd444480f6d1da035ef0
Jean Connelly <jconnelly@cfa.harvard.edu>
2012-02-02 14:04:12 -0500

    Added support for times in-between timelines

--- starcheck_at.py
+++ starcheck_at.py
@@ -3,15 +3,27 @@
 import sys
 from Ska.DBI import DBI
 from Chandra.Time import DateTime
+import numpy as np
 
 dbh = DBI(dbi='sybase', server='sybase', user='aca_read')
+mp = '/data/mpcrit1/mplogs'
 
 t = DateTime()
 if len(sys.argv) > 1:
     t = DateTime(sys.argv[1])
-timeline = dbh.fetchone("""select * from timelines \
-                           where datestart <= '%s'
-                           and datestop > '%s'"""
-                           % (t.date, t.date))
-mp = '/data/mpcrit1/mplogs'
-print "file://%s%s%s" % (mp, timeline['dir'], 'starcheck.html')
+timelines = dbh.fetchall(
+"""select * from timelines
+   where datestart <= '%(date)s' and datestop > '%(date)s'"""
+% {'date': t.date})
+
+if not len(timelines):
+    timelines = dbh.fetchall(
+"""select * from timelines
+   where ( datestart = (
+       select max(datestart) from timelines where datestart < '%(date)s'))
+    or ( datestart = (
+       select min(datestart) from timelines where datestart > '%(date)s'))"""
+% {'date': t.date})
+
+for tdir in np.unique(timelines['dir']):
+    print "file://%s%s%s" % (mp, tdir, 'starcheck.html')
